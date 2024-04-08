7a0cafdc52b5997fecdf96893e5d72d7b4046994
iosHumhae <ios.humhae@gmail.com>
2024-04-08 10:47:25 +0900

    feat: 패키지 Firebase 의존성 추가

--- ZzritKit/Package.swift
+++ ZzritKit/Package.swift
@@ -11,11 +11,20 @@ let package = Package(
             name: "ZzritKit",
             targets: ["ZzritKit"]),
     ],
+    dependencies: [
+        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", .upToNextMajor(from: "10.23.0"))
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "ZzritKit"),
+            name: "ZzritKit",
+            dependencies: [
+                .product(name: "FirebaseFirestore", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseStorage", package: "firebase-ios-sdk"),
+                .product(name: "FirebaseAuth", package: "firebase-ios-sdk")
+            ]
+        ),
         .testTarget(
             name: "ZzritKitTests",
             dependencies: ["ZzritKit"]),
