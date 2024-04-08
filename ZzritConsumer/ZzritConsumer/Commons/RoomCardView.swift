eb6336958d6a5c7cf6ff53d75c64a860c8bffaa9
Hayunho <yunho081544@gmail.com>
2024-04-08 16:21:49 +0900

    feat: 로고이미지 및 더미 이미지 에셋 추가

---
+++ ZzritConsumer/ZzritConsumer/Commons/RoomCardView.swift
@@ -0,0 +1,20 @@
+//
+//  RoomCardView.swift
+//  ZzritConsumer
+//
+//  Created by 하윤호 on 4/8/24.
+//
+
+import SwiftUI
+
+struct RoomCardView: View {
+    var body: some View {
+        VStack {
+            
+        }
+    }
+}
+
+#Preview {
+    RoomCardView()
+}
