25cf83369caf730d354bfb431dd368f0ccfe873a
iosHumhae <ios.humhae@gmail.com>
2024-04-08 09:08:29 +0900

    init project

---
+++ ZzritAdministrator/ZzritAdministrator/ZzritAdministratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZzritAdministratorApp.swift
+//  ZzritAdministrator
+//
+//  Created by Sanghyeon Park on 4/8/24.
+//
+
+import SwiftUI
+
+@main
+struct ZzritAdministratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ZzritConsumer/ZzritConsumer/ZzritConsumerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZzritConsumerApp.swift
+//  ZzritConsumer
+//
+//  Created by Sanghyeon Park on 4/8/24.
+//
+
+import SwiftUI
+
+@main
+struct ZzritConsumerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
