1c563b324c25d7d9f671fe8ddde9748de8b82ebc
JaeWoongJeong <jjw8959@naver.com>
2024-04-08 15:52:04 +0900

    fix: enum 이름 수정 및 파일 추가
    enum 타입들의 이름을 *Type으로 수정했습니다. 카테고리를 enum 타입으로 선언해 RoomModel을 수정했습니다.

---
+++ ZzritKit/Sources/ZzritKit/Enum/ActiveType.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  
+//
+//  Created by woong on 4/8/24.
+//
+
+import Foundation
+
+public enum ActiveType: Codable {
+    case active
+    case deactive
+}
---
+++ ZzritKit/Sources/ZzritKit/Enum/PlatformType.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  
+//
+//  Created by woong on 4/8/24.
+//
+
+import Foundation
+
+public enum PlatformType: String, Codable, CaseIterable {
+    case zoom = "줌"
+    case googleMeet = "구글미트"
+    case discord = "디스코드"
+    case etc = "기타"
+}
