93c6419d6bc635c7c9d63b9ce908ef22b1a0efd8
iosHumhae <ios.humhae@gmail.com>
2024-04-09 02:05:21 +0900

    fix: Contact 모델 이니셜라이저 추가

--- ZzritKit/Sources/ZzritKit/Model/ContactModel.swift
+++ ZzritKit/Sources/ZzritKit/Model/ContactModel.swift
@@ -21,6 +21,18 @@ public struct ContactModel: Codable, Identifiable {
     public var targetUser: [String]?
     public var latestAnswerDate: Date?
     
+    public init(id: String? = nil, category: ContactCategory, title: String, content: String, requestedDated: Date, requestedUser: String, targetRoom: String? = nil, targetUser: [String]? = nil, latestAnswerDate: Date? = nil) {
+        self.id = id
+        self.category = category
+        self.title = title
+        self.content = content
+        self.requestedDated = requestedDated
+        self.requestedUser = requestedUser
+        self.targetRoom = targetRoom
+        self.targetUser = targetUser
+        self.latestAnswerDate = latestAnswerDate
+    }
+    
     public var isAnswered: Bool {
         if let _ = latestAnswerDate {
             return true
