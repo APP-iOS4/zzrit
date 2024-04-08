996832676339d8bdd95c28540b166e8302be0897
Hayunho <yunho081544@gmail.com>
2024-04-08 18:49:32 +0900

    feat: 모임 리스트 셀 뷰 구현

---
+++ ZzritConsumer/ZzritConsumer/Main/RoomCellView.swift
@@ -0,0 +1,18 @@
+//
+//  RoomCellView.swift
+//  ZzritConsumer
+//
+//  Created by 하윤호 on 4/8/24.
+//
+
+import SwiftUI
+
+struct RoomCellView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RoomCellView()
+}
