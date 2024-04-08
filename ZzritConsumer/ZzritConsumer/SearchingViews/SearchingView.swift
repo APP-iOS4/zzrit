2177bc1762d56747a5088bcb3c4993cdf0461fe5
SunJuneL <sunjoon96@gmail.com>
2024-04-08 15:19:08 +0900

    feat: Consumer Config 구조체와 RecentSearchCellView(최근 검색어 셀 뷰) 생성

---
+++ ZzritConsumer/ZzritConsumer/SearchingViews/SearchingView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchingView.swift
+//  ZzritConsumer
+//
+//  Created by 이선준 on 4/8/24.
+//
+
+import SwiftUI
+
+struct SearchingView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    SearchingView()
+}
