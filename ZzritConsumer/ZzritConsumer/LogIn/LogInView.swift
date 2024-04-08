24564dde9cfce6b8c6589507861c40daa2bd0fca
yeahz <hpns1379@naver.com>
2024-04-08 20:33:15 +0900

    fix: subview 분할
    파일 찢어놓음.

--- ZzritConsumer/ZzritConsumer/LogIn/LogInView.swift
+++ ZzritConsumer/ZzritConsumer/LogIn/LogInView.swift
@@ -7,18 +7,6 @@
 
 import SwiftUI
 import ZzritKit
-struct AppSlogan: View {
-    var body: some View {
-        Text("만나는 순간의 짜릿함")
-            .font(.title3)
-            .fontWeight(.bold)
-            .foregroundStyle(Color.staticGray1)
-        Text("ZZ!LIT")
-            .font(.largeTitle)
-            .fontWeight(.heavy)
-            .foregroundStyle(Color.pointColor)
-    }
-}
 
 struct LogInView: View {
     
--- ZzritConsumer/ZzritConsumer/LogIn/SignUpView.swift
+++ ZzritConsumer/ZzritConsumer/LogIn/SignUpView.swift
@@ -164,28 +164,6 @@ struct SignUpView: View {
     }
 }
 
-struct SignUpAgreeButton: View {
-    @Binding var selectAgree: Bool
-    var body: some View {
-        HStack {
-            if selectAgree{
-                Image(systemName: "checkmark.circle")
-                    .font(.title3)
-                    .fontWeight(.medium)
-                    .foregroundStyle(Color.pointColor)
-            } else{
-                Image(systemName: "circle")
-            }
-            Text("서비스 이용약관 동의")
-            Spacer()
-            Text("보기")
-                .underline()
-        }
-        .foregroundStyle(Color.staticGray2)
-    }
-    
-}
-
 #Preview {
     SignUpView()
 }
