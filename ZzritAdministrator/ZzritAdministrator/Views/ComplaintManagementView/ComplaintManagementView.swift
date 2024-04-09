1a6bea323c0ba7087cd7bdfacd5a78d0a3af8a10
Wooseok Lee <wl00ie19@gmail.com>
2024-04-09 17:45:43 +0900

    feat: 문의 모델 ZzritKit의 ContactModel로 변경, 신고에서 문의로 용어 변경

--- ZzritAdministrator/ZzritAdministrator/Views/ComplaintManagementView/ComplaintManagementView.swift
+++ ZzritAdministrator/ZzritAdministrator/Views/ComplaintManagementView/ComplaintManagementView.swift
@@ -5,25 +5,27 @@
 //  Created by Healthy on 4/8/24.
 //
 
+// TODO: Complaint가 들어간 뷰 파일명 모델에 맞추어 Contact로 변경, ContentView 목록의 신고도 문의로 변경 필요
+
 import SwiftUI
 
 import ZzritKit
 
 struct ComplaintManagementView: View {
-    @State private var qnaCategory: TempQnaType = .allCase
-    @State private var qnaSearchText: String = ""
-    @State var pickQnaId: QnaStruct.ID?
+    @State private var contactCategory: ContactCategory = .app
+    @State private var contactSearchText: String = ""
+    @State var pickedContact: ContactModel?
     @State private var isShowingModalView = false
     
     var body: some View {
         VStack {
             HStack {
-                QnaReasonPickerView(selectReason: $qnaCategory)
-                TextField("신고 내용을 입력해주세요.", text: $qnaSearchText)
+                ContactReasonPickerView(selectReason: $contactCategory)
+                TextField("문의 내용을 입력해주세요.", text: $contactSearchText)
                     .padding(10.0)
                     .padding(.leading)
                 Button {
-                    print("검색! with (qnaSearchText)")
+                    print("검색!")
                 } label: {
                     Image(systemName: "magnifyingglass")
                         .foregroundStyle(.white)
@@ -37,13 +39,13 @@ struct ComplaintManagementView: View {
                     .foregroundStyle(.white)
                     .shadow(radius: 10)
             }
-            List(tempQnaList){ list in
+            List(tempContactList){ list in
                 Button {
-                    pickQnaId = list.id
+                    pickedContact = list
                     isShowingModalView.toggle()
                     print("modal .toggle     pickUserId = list.id")
                 } label: {
-                    QnaListCell(qnaTitle: list.qnaName, qnaCategory: list.qnaCategory, qnaDate: list.qnaDate)
+                    ContactListCell(contactTitle: list.title, contactCategory: list.category, contactDate: list.requestedDated)
                 }
             }
             .listStyle(.inset)
@@ -54,42 +56,45 @@ struct ComplaintManagementView: View {
         }
         .padding()
         .fullScreenCover(isPresented: $isShowingModalView, content: {
-            ComplaintDetailView(qnaId: $pickQnaId, isShowingModalView: $isShowingModalView)
+            ComplaintDetailView(contact: $pickedContact, isShowingModalView: $isShowingModalView)
         })
     }
 }
 
-struct QnaReasonPickerView: View {
-    @Binding var selectReason: TempQnaType
+struct ContactReasonPickerView: View {
+    @Binding var selectReason: ContactCategory
     var body: some View {
         Picker("\(selectReason)", selection: $selectReason){
-            Text("문의 종류").tag(TempQnaType.allCase)
-            Text("폭언/욕설 사용").tag(TempQnaType.abuse)
-            Text("부적절한 모임 개설").tag(TempQnaType.wrongRoom)
-            Text("종교 권유").tag(TempQnaType.religion)
-            Text("불법 도박 홍보").tag(TempQnaType.gambling)
-            Text("음란성 모임 개설").tag(TempQnaType.obscenity)
-            Text("기타 사유").tag(TempQnaType.administrator)
+            // Text("문의 종류").tag(ContactCategory.allCases)
+            Text("앱 이용 문의").tag(ContactCategory.app)
+            Text("회원 신고").tag(ContactCategory.mamber)
+            Text("모임 신고").tag(ContactCategory.room)
         }
         .pickerStyle(.menu)
         .tint(Color.pointColor)
     }
 }
 
-struct QnaListCell: View {
-    var qnaTitle: String
-    var qnaCategory: TempQnaType
-    var qnaDate: String
+struct ContactListCell: View {
+    var contactTitle: String
+    var contactCategory: ContactCategory
+    var contactDate: Date
+    var contactDateString: String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        let dateString: String = formatter.string(from: contactDate)
+        return dateString
+    }
     var body: some View {
         VStack(alignment: .leading, spacing: 10) {
-            Text("\(qnaTitle)")
+            Text("\(contactTitle)")
                 .fontWeight(.bold)
                 .foregroundStyle(.black)
             HStack {
-                Text("\(qnaCategory.rawValue)")
+                Text("\(contactCategory.rawValue)")
                     .fontWeight(.bold)
                     .foregroundStyle(Color.pointColor)
-                Text("\(qnaDate)")
+                Text("\(contactDateString)")
                     .foregroundStyle(Color.staticGray2)
             }
         }
@@ -97,31 +102,13 @@ struct QnaListCell: View {
     }
 }
 
-public enum TempQnaType: String, CaseIterable {
-    case allCase = "문의 종류"
-    case abuse = "폭언/욕설 사용"
-    case wrongRoom = "부적절한 모임 개설"
-    case religion = "종교 권유"
-    case gambling = "불법 도박 홍보"
-    case obscenity = "음란성 모임 개설"
-    case administrator = "기타 사유"
-}
-
-struct QnaStruct: Identifiable {
-    var id: UUID = UUID()
-    
-    let qnaName: String
-    let qnaCategory: TempQnaType
-    let qnaDate: String
-    let qnaContent: String
-    let qnaWriter: UUID
-}
-
-let tempQnaList: [QnaStruct] = [
-    .init(qnaName: "집단으로 성적 수치심을 주는 모임", qnaCategory: .obscenity, qnaDate: "2024-04-01", qnaContent: "모임에서 저에게 집단으로 성적 수치심을 주는 채팅을 보내요.", qnaWriter: UUID()),
-    .init(qnaName: "불법도박 위장모임", qnaCategory: .gambling, qnaDate: "2024-04-01", qnaContent: "자꾸만 불법도박사이트 링크를 보내요.", qnaWriter: UUID()),
-    .init(qnaName: "단체 욕설", qnaCategory: .abuse, qnaDate: "2024-04-01", qnaContent: "다른 멤버들이 저를 따돌리고 제 부모님을 욕했어요.", qnaWriter: UUID()),
-    .init(qnaName: "사이비종교 포교", qnaCategory: .religion, qnaDate: "2024-04-01", qnaContent: "게임 모임인것처럼 위장해서 자꾸만 신천치로 의심되는 비인가 성경공부 강요해요.", qnaWriter: UUID())
+let tempContactList: [ContactModel] = [
+    .init(category: .room, title: "집단으로 성적 수치심을 주는 모임 신고", content: "모임에서 저에게 집단으로 성적 수치심을 주는 채팅을 보내요.", requestedDated: Date(), requestedUser: "abcd1@example.com", targetRoom: "신촌에서 맥주마실 분 구합니다"),
+    .init(category: .room, title: "집단따돌림 모임 신고", content: "다른 멤버들이 저를 따돌리고 제 부모님을 욕했어요.", requestedDated: Date(), requestedUser: "abcd2@example.com", targetRoom: "배틀그라운드 초보자 모임"),
+    .init(category: .mamber, title: "사이비종교 포교 사용자 신고", content: "모임 내에서 자꾸만 세계종말론 설파하며 비인가 성경공부 강요해요.", requestedDated: Date(), requestedUser: "abcd3@example.com", targetUser: ["saibi@example.com"]),
+    .init(category: .mamber, title: "정치 선동 사용자 신고", content: "자꾸만 특정 정당 후보 투표 강요하고 타 정당 비하발언해요. ", requestedDated: Date(), requestedUser: "abcd4@example.com", targetUser: ["politician@example.com", "jeongdang@example.com", "voteme@example.com"]),
+    .init(category: .app, title: "앱 오류 신고", content: "모임 생성이 안 돼요.", requestedDated: Date(), requestedUser: "abcd4@example.com"),
+    .init(category: .app, title: "제재 해제 문의", content: "계정이 해킹당해서 도박사이트 링크가 뿌려졌습니다. 복구 가능할까요?", requestedDated: Date(), requestedUser: "abcd4@example.com"),
 ]
 
 struct writerUser: Identifiable {
@@ -132,7 +119,6 @@ struct writerUser: Identifiable {
     let birthYear: Int
     let gender: GenderType
 }
-
 
 #Preview {
     ComplaintManagementView()
