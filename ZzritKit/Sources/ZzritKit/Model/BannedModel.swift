92345873473176587b58fa0afcfedf6823d38863
iosHumhae <ios.humhae@gmail.com>
2024-04-09 11:17:15 +0900

    docs: 모델의 주석 추가

--- ZzritKit/Sources/ZzritKit/Model/BannedModel.swift
+++ ZzritKit/Sources/ZzritKit/Model/BannedModel.swift
@@ -12,7 +12,10 @@ import FirebaseFirestore
 /// 이용정지 모델
 public struct BannedModel: Codable, Identifiable {
     @DocumentID public var id: String?
+    /// 이용정지 시작 날짜
     public var date: Date
+    /// 이용정지 만료 날짜
     public var period: Date
+    /// 이용 정지 타입
     public var type: BannedType
 }
--- ZzritKit/Sources/ZzritKit/Model/ContactReplyModel.swift
+++ ZzritKit/Sources/ZzritKit/Model/ContactReplyModel.swift
@@ -12,12 +12,17 @@ import FirebaseFirestore
 /// 문의사항 답변 모델
 public struct ContactReplyModel: Codable, Identifiable {
     @DocumentID public var id: String?
+    /// 문의사항 답변 등록 날짜
     public var date: Date
+    /// 문의사항 답변 내용
     public var content: String
+    /// 문의사항 답변 등록 관리자 uid
+    public var answeredAdmin: String
     
-    public init(id: String? = nil, date: Date, content: String) {
+    public init(id: String? = nil, date: Date, content: String, answeredAdmin: String) {
         self.id = id
         self.date = date
         self.content = content
+        self.answeredAdmin = answeredAdmin
     }
 }
