ec5c21370a392c3a2ffb5ece6b3cedc021dfdc4b
Hayunho <yunho081544@gmail.com>
2024-04-09 14:05:51 +0900

    feat: 모임 상세 설명 구현

---
+++ ZzritConsumer/ZzritConsumer/RoomDetails/RoomDetailView.swift
@@ -0,0 +1,49 @@
+//
+//  RoomDetailView.swift
+//  ZzritConsumer
+//
+//  Created by 하윤호 on 4/9/24.
+//
+
+import SwiftUI
+
+struct RoomDetailView: View {
+    
+    // MARK: - body
+    
+    var body: some View {
+        ScrollView {
+            LazyVStack(alignment: .leading) {
+                // 상단 타이틀 Stack
+                HStack {
+                    // 카테고리
+                    RoomCategoryView("술벙")
+                    
+                    // 타이틀
+                    Text("같이 모여서 가볍게 치맥하실 분 구해요!")
+                        .lineLimit(1)
+                        .font(.title3)
+                }
+                
+                // 썸네일 이미지
+                Image(.dummy)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .clipShape(RoundedRectangle(cornerRadius: Configs.cornerRadius))
+                
+                // 세부 내용
+                Text("여기는 세부적인 내용이 들어가는 곳입니다. 주제와 관련된 내용을 작성해 주시기 바랍니다.")
+                    .foregroundStyle(Color.staticGray1)
+                
+                // 위치, 시간, 참여 인원에 대한 정보를 나타내는 뷰
+                // TODO: 모임 모델 하나를 이 곳에 전달을 해주어야 함
+                RoomInfoView()
+            }
+            .padding(20)
+        }
+    }
+}
+
+#Preview {
+    RoomDetailView()
+}
