3c9b30f163bb772cf3349e853f666580b87d9c0b
Hayunho <yunho081544@gmail.com>
2024-04-09 15:23:53 +0900

    feat: RoomDetailView 구현

--- ZzritConsumer/ZzritConsumer/RoomDetails/RoomDetailView.swift
+++ ZzritConsumer/ZzritConsumer/RoomDetails/RoomDetailView.swift
@@ -8,11 +8,13 @@
 import SwiftUI
 
 struct RoomDetailView: View {
+    // 참석 버튼 눌렀는 지 확인
+    @State private var isParticipant: Bool = false
     
     // MARK: - body
     
     var body: some View {
-        ZStack {
+        ZStack(alignment: .bottom) {
             ScrollView {
                 LazyVStack(alignment: .leading) {
                     // 상단 타이틀 Stack
@@ -22,7 +24,6 @@ struct RoomDetailView: View {
                         
                         // 타이틀
                         Text("같이 모여서 가볍게 치맥하실 분 구해요!")
-                            .lineLimit(1)
                             .font(.title3)
                     }
                     
@@ -30,15 +31,19 @@ struct RoomDetailView: View {
                     Image(.dummy)
                         .resizable()
                         .aspectRatio(contentMode: .fill)
+                        .frame(maxHeight: 200)
                         .clipShape(RoundedRectangle(cornerRadius: Configs.cornerRadius))
+                        .padding(.bottom, 20)
                     
                     // 세부 내용
                     Text("여기는 세부적인 내용이 들어가는 곳입니다. 주제와 관련된 내용을 작성해 주시기 바랍니다.")
                         .foregroundStyle(Color.staticGray1)
+                        .padding(.bottom, 40)
                     
                     // 위치, 시간, 참여 인원에 대한 정보를 나타내는 뷰
                     // TODO: 모임 모델 하나를 이 곳에 전달을 해주어야 함
                     RoomInfoView()
+                        .padding(.bottom, 40)
                     
                     Text("와우, 벌써 4명이나 모였어요.")
                         .font(.title3)
@@ -48,6 +53,18 @@ struct RoomDetailView: View {
                     ParticipantListView()
                 }
                 .padding(20)
+            }
+            .padding(.bottom, 70)
+            
+            VStack {
+                GeneralButton("참여하기") {
+                    isParticipant.toggle()
+                }
+                .padding(20)
+                .navigationDestination(isPresented: $isParticipant) {
+                    // TODO: 여기 안내문 뷰 나오는 대로 연결해 주어야 함
+                    Text("안내문 뷰")
+                }
             }
         }
         .toolbarRole(.editor)
