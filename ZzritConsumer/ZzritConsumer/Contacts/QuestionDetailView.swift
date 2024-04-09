9b22ebda711e8819d03afd52dc823fb4ee2bd4f3
Hayunho <101854288+Hayunho@users.noreply.github.com>
2024-04-09 22:43:19 +0900

    feat: 문의하기 뷰 구현

---
+++ ZzritConsumer/ZzritConsumer/Contacts/QuestionDetailView.swift
@@ -0,0 +1,33 @@
+//
+//  QuestionDetailView.swift
+//  ZzritConsumer
+//
+//  Created by 하윤호 on 4/9/24.
+//
+
+import SwiftUI
+
+struct QuestionDetailView: View {
+    var body: some View {
+        ScrollView {
+            LazyVStack {
+                // 문의내역 질문 뷰
+                // FIXME: 모델 연동 시 isAnswered가 아닌 모델 받는 것으로 수정해야 함
+                ContactQuestionDetailView(isAnswered: true)
+                    .padding(.bottom, 40)
+                
+                Divider()
+                
+                // 문의내역 답변 뷰
+                ContactReplyDetailView()
+                    .padding(.top, Configs.paddingValue)
+            }
+            .padding(.horizontal, 20)
+        }
+        
+    }
+}
+
+#Preview {
+    QuestionDetailView()
+}
