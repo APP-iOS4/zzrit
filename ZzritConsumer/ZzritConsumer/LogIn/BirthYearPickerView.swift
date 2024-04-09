5506b691e0b8dbdcb6ba98ca32e9008646e89175
yeahz <hpns1379@naver.com>
2024-04-09 12:41:49 +0900

    feat: PickerView스타일 수정
    sheet타입으로 띄우도록 수정

--- ZzritConsumer/ZzritConsumer/LogIn/BirthYearPickerView.swift
+++ ZzritConsumer/ZzritConsumer/LogIn/BirthYearPickerView.swift
@@ -9,14 +9,25 @@ import SwiftUI
 
 struct BirthYearPickerView: View {
     @Binding var selectedYear: Int
+    @Environment (\.dismiss) private var dismiss
     
     var body: some View {
-        Picker("Choose a color", selection: $selectedYear) {
-            ForEach(1800...Calendar.current.component(.year, from: Date()), id: \.self) { year in
-                Text(String(year))
+        VStack {
+            Picker("출생년도를 선택해주세요.", selection: $selectedYear) {
+                ForEach(1800...Calendar.current.component(.year, from: Date()), id: \.self) { year in
+                    Text(String(year))
+                }
             }
+            .pickerStyle(.wheel)
+            Button(action: {
+                dismiss()
+            }, label: {
+                Text("선택 완료")
+                    .foregroundStyle(Color.pointColor)
+            })
         }
-        .pickerStyle(.wheel)
+        .presentationDetents([.height(250)])
+        .interactiveDismissDisabled()
     }
     
 }
