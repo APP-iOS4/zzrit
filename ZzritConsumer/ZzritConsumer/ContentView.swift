4b0e7ec0ce43b126af02a1ea95ff7d8a6440fa97
yeahz <hpns1379@naver.com>
2024-04-10 15:51:52 +0900

    feat: ContentView에 TabView 추가

--- ZzritConsumer/ZzritConsumer/ContentView.swift
+++ ZzritConsumer/ZzritConsumer/ContentView.swift
@@ -6,16 +6,27 @@
 //
 
 import SwiftUI
+import ZzritKit
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            MainView()
+                .tabItem { Label("모임", systemImage: "house.fill") }
+            
+            SearchingView()
+                .tabItem { Label("탐색", systemImage: "magnifyingglass") }
+            
+            // TODO: 채팅 연결
+            
+//            ChattingView()
+//                .tabItem { Label("채팅", systemImage: "ellipsis.bubble") }
+            
+            MoreInfoView()
+                .tabItem { Label("더보기", systemImage: "ellipsis.circle") }
         }
-        .padding()
+        .tint(Color.pointColor)
+        
     }
 }
 
