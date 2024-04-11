7f35295f6630851bc2eda5b878069c147bc8b91d
Hayunho <yunho081544@gmail.com>
2024-04-11 21:36:55 +0900

    feat: ChatMessageCellView 자신 메세지 뷰 뼈대 구현

--- ZzritConsumer/ZzritConsumer/Chats/ChatView.swift
+++ ZzritConsumer/ZzritConsumer/Chats/ChatView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct ChatView: View {
+    // 입력 메세지 변수
+    @State private var messageText: String = ""
     //임시 뷰모델 호출
     let messageViewModel: MessageViewModel = MessageViewModel()
     
@@ -16,10 +18,12 @@ struct ChatView: View {
             ScrollView {
                 LazyVStack {
                     ForEach(messageViewModel.messages) { message in
+                        // 메세지가 상대방일 경우
                         if !message.isYou {
                             LazyVStack(alignment: .leading) {
                                 ChatMessageCellView(message: message)
                             }
+                        //  메세지가 내꺼일 경우
                         } else {
                             LazyVStack(alignment: .trailing) {
                                 ChatMessageCellView(message: message)
