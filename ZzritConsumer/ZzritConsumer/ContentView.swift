54a943ce7134334f2c663cf0f8f05fcfceb65b14
Hayunho <yunho081544@gmail.com>
2024-04-11 09:07:03 +0900

    fix: 탭바 수정
    - 선택된 탭 아이콘 fill로 되도록 수정

--- ZzritConsumer/ZzritConsumer/ContentView.swift
+++ ZzritConsumer/ZzritConsumer/ContentView.swift
@@ -9,20 +9,33 @@ import SwiftUI
 import ZzritKit
 
 struct ContentView: View {
+    @State private var tabSelection: Int = 0
+    
     var body: some View {
-        // TODO: icon 선택시 fill로 
-        TabView {
+        TabView(selection: $tabSelection) {
             MainView()
-                .tabItem { Label("모임", image: "home") }
+                .tabItem { 
+                    Label("모임", image: tabSelection == 0 ? "homefill" : "home")
+                }
+                .tag(0)
             
             SearchingView()
-                .tabItem { Label("탐색", image: "search") }
+                .tabItem {
+                    Label("탐색", image: "search")
+                }
+                .tag(1)
             
             ChatListView()
-                .tabItem { Label("채팅", image: "chat") }
+                .tabItem {
+                    Label("채팅", image: tabSelection == 2 ? "chatfill" : "chat")
+                }
+                .tag(2)
             
             MoreInfoView()
-                .tabItem { Label("더보기", image: "moreInfo") }
+                .tabItem {
+                    Label("더보기", image: tabSelection == 3 ? "moreInfofill" : "moreInfo")
+                }
+                .tag(3)
         }
         .tint(Color.pointColor)
     }
