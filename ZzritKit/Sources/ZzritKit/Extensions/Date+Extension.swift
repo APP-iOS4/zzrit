4583f34ef0707b7d4c4f34383cb5d2341ce0ff49
JaeWoongJeong <jjw8959@naver.com>
2024-04-12 10:52:29 +0900

    feat: Date와 String간의 변환 기능 구현
    날짜를 Date 와 String 간에 자유롭게 주고받을 수 있게 변환하는 기능 구현 완료

--- ZzritKit/Sources/ZzritKit/Extensions/Date+Extension.swift
+++ ZzritKit/Sources/ZzritKit/Extensions/Date+Extension.swift
@@ -11,4 +11,15 @@ extension Date {
     func addDay(at: Int) -> Date {
         return Calendar.current.date(byAdding: .day, value: at, to: self)!
     }
+    /**
+     - Date를 String으로 바꿔주는 메서드
+     - 문자열 끝에 .toString()로 쓰면 String을 반환.
+     - 문자열의 형식은 "2024-03-01 08:35:40" 형태로 반환됨.
+     */
+    public func toString() -> String {
+        let df = DateFormatter()
+        df.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        df.timeZone = TimeZone(identifier: "UTC")
+        return df.string(from: self)
+    }
 }
