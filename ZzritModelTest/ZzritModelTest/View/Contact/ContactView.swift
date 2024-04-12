254bb413385f3e3ca33e3c72cea682760d0c750d
iosHumhae <ios.humhae@gmail.com>
2024-04-12 14:09:46 +0900

    refactor: 문의사항 테스트뷰 분리

--- ZzritModelTest/ZzritModelTest/View/Contact/ContactView.swift
+++ ZzritModelTest/ZzritModelTest/View/Contact/ContactView.swift
@@ -15,54 +15,25 @@ struct ContactView: View {
     @State private var contacts: [ContactModel] = []
     @State private var isInitial: Bool = true
     
-    @State private var contactSubject: String = ""
-    @State private var contactContent: String = ""
-    private var categories: [ContactCategory] = ContactCategory.allCases
-    @State private var selectedCategory: ContactCategory = .app
-    
-    var writeDisabled: Bool {
-        return (contactSubject == "") || (contactContent == "")
-    }
-    
     var body: some View {
-        
-        // MARK: 문의사항 목록 (모든회원)
-        
-        List {
-            ForEach(contacts) { contact in
-                Text(contact.title)
+        NavigationStack {
+            List {
+                ForEach(contacts) { contact in
+                    Text(contact.title)
+                }
+                .onDelete { indexSet in
+                    deleteContact(indexSet: indexSet)
+                }
             }
-            .onDelete { indexSet in
-                deleteContact(indexSet: indexSet)
+            .onAppear {
+                fetchContacts()
             }
-        }
-        .onAppear {
-            fetchContacts()
-        }
-        
-        // MARK: 문의사항 작성
-        
-        Form {
-            Section("문의 작성") {
-                TextField("문의 제목", text: $contactSubject)
-                TextEditor(text: $contactContent)
-                    .overlay {
-                        if contactContent == "" {
-                            Text("문의 내용")
-                                .foregroundStyle(.placeholder)
-                                .frame(maxWidth: .infinity, alignment: .leading)
-                        }
-                    }
-                Picker("문의 카테고리", selection: $selectedCategory) {
-                    ForEach(categories, id: \.self) { category in
-                        Text(category.rawValue)
-                            .tag(category)
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    NavigationLink("등록") {
+                        ContactWriteView()
                     }
                 }
-                Button("작성") {
-                    writeContact()
-                }
-                .disabled(writeDisabled)
             }
         }
     }
@@ -75,20 +46,6 @@ struct ContactView: View {
             } catch {
                 print("에러: \(error)")
             }
-        }
-    }
-    
-    private func writeContact() {
-        let requestedUser = "28IsmsvqGYd0DNrS3tjkuUVzr2l2"
-        
-        let tempModel: ContactModel = .init(category: selectedCategory, title: contactSubject, content: contactContent, requestedDated: Date(), requestedUser: requestedUser)
-        
-        do {
-            try contactService.writeContact(tempModel)
-            contactSubject = ""
-            contactContent = ""
-        } catch {
-            print("에러: \(error)")
         }
     }
     
