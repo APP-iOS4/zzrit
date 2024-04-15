109c84e049bcaa91b79cc5a1ba0f072743fd017b
Wooseok Lee <wl00ie19@gmail.com>
2024-04-15 10:31:19 +0900

    fix: 신규 모델 대응 수정 및 텍스트 컬러 변경(다크모드 대응)

---
+++ ZzritAdministrator/ZzritAdministrator/Views/TermsView/TermsView.swift
@@ -0,0 +1,18 @@
+//
+//  TermsView.swift
+//  ZzritAdministrator
+//
+//  Created by 이우석 on 4/15/24.
+//
+
+import SwiftUI
+
+struct TermsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TermsView()
+}
