832f82875c468da6d6ec67771cfc2a13fde6921c
iosHumhae <ios.humhae@gmail.com>
2024-04-17 21:40:42 +0900

    refactor: 나의 정전기 지수 타이틀 디테일

--- ZzritConsumer/ZzritConsumer/MoreInfo/StaticGuageView/MyStaticGaugeView.swift
+++ ZzritConsumer/ZzritConsumer/MoreInfo/StaticGuageView/MyStaticGaugeView.swift
@@ -8,15 +8,19 @@
 import SwiftUI
 
 struct MyStaticGaugeView: View {
+    let staticPoint: Double
+    
     var body: some View {
         VStack(alignment: .leading) {
             Text("나의 정전기 지수")
-                .font(.title2)
+                .font(.title3)
+                .fontWeight(.bold)
+                .frame(maxWidth: .infinity, alignment: .leading)
             
             // TODO: (staticGauge: 66) 사용자의 정전기지수 넣어주기
             // 애니메이션 효과 넣어주기
             
-            StaticGaugeProgressView(staticGauge: 0)
+            StaticGaugeProgressView(staticGauge: staticPoint)
             VStack(alignment: .center) {
                 Text("정전기 지수가 낮으면 사람들과 연결되기 힘들어요!")
                 Text("낮아지지 않도록 매너있게 행동합시다!")
@@ -31,6 +35,6 @@ struct MyStaticGaugeView: View {
     }
 }
 
-#Preview {
-    MyStaticGaugeView()
-}
+//#Preview {
+//    MyStaticGaugeView()
+//}
