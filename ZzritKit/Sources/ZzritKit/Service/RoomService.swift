4a625c06bafd211b855408d7d447ab6e4fc00c54
JaeWoongJeong <jjw8959@naver.com>
2024-04-17 17:38:10 +0900

    fix: 코멘트에 따른 수정

--- ZzritKit/Sources/ZzritKit/Service/RoomService.swift
+++ ZzritKit/Sources/ZzritKit/Service/RoomService.swift
@@ -96,7 +96,7 @@ public final class RoomService {
                 
                 return temp
                 
-            } else if let titleString = title, (titleString == "") {
+            } else if let titleString = title, titleString == "" {
                 
                 if !isInitial, let lastDocument = lastSnapshot {
                     query = query.start(afterDocument: lastDocument)
