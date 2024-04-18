202629b4d9e544e8d8b4884970ac57d4a653aa2e
Hayunho <yunho081544@gmail.com>
2024-04-18 16:09:10 +0900

    feat: 모임 상세뷰 기능 추가
    - 참여한 방이라면 버튼 비활성화

--- ZzritConsumer/ZzritConsumer/RoomDetails/RoomDetailView.swift
+++ ZzritConsumer/ZzritConsumer/RoomDetails/RoomDetailView.swift
@@ -21,6 +21,8 @@ struct RoomDetailView: View {
     
     @State private var isShowingLoginView: Bool = false
     
+    @State private var isJoined: Bool = false
+    
     @State private var participants: [JoinedUserModel] = []
     
     @State private var participantsCount: Int = 0
@@ -34,70 +36,168 @@ struct RoomDetailView: View {
     // MARK: - body
     
     var body: some View {
-        ZStack(alignment: .bottom) {
-            ScrollView {
-                LazyVStack(alignment: .leading) {
-                    // 상단 타이틀 Stack
-                    HStack {
-                        // 카테고리
-                        RoomCategoryView(room.category.rawValue)
-                        
-                        // 타이틀
-                        Text(room.title)
-                            .font(.title3)
-                    }
-                    
-                    // 썸네일 이미지
-                    RoundedRectangle(cornerRadius: 10)
-                        .foregroundStyle(.clear)
-                        .aspectRatio(contentMode: .fill)
-                        .frame(maxHeight: 200)
-                        .background {
-                            AsyncImage(url: room.roomImage) { image in
-                                image
-                                    .resizable()
-                                    .aspectRatio(contentMode: .fill)
-                                    .frame(maxHeight: 200)
-                                    .clipShape(RoundedRectangle(cornerRadius: Configs.cornerRadius))
-                                    
-                            } placeholder: {
-                                ProgressView()
-                                    .frame(maxWidth: .infinity, alignment: .center)
-                            }
+        
+        //MARK: - iOS17
+        
+        if #available(iOS 17.0, *) {
+            ZStack(alignment: .bottom) {
+                ScrollView {
+                    LazyVStack(alignment: .leading) {
+                        // 상단 타이틀 Stack
+                        HStack {
+                            // 카테고리
+                            RoomCategoryView(room.category.rawValue)
+                            
+                            // 타이틀
+                            Text(room.title)
+                                .font(.title3)
                         }
-                        .padding(.bottom, 20)
-    
-                    // 세부 내용
-                    Text(room.content)
-                        .foregroundStyle(Color.staticGray1)
-                        .padding(.bottom, 40)
-                    
-                    // 위치, 시간, 참여 인원에 대한 정보를 나타내는 뷰
-                    RoomInfoView(room: room, participantsCount: participantsCount)
-                        .padding(.bottom, 40)
-                    
-                    Text("와우, 벌써 \(participantsCount)명이나 모였어요.")
-                        .font(.title3)
-                        .fontWeight(.bold)
-                    // 참여자의 정보를 나타내는 뷰
-                    ParticipantListView(room: room, participants: participants)
+                        
+                        // 썸네일 이미지
+                        RoundedRectangle(cornerRadius: 10)
+                            .foregroundStyle(.clear)
+                            .aspectRatio(contentMode: .fill)
+                            .frame(maxHeight: 200)
+                            .background {
+                                AsyncImage(url: room.roomImage) { image in
+                                    image
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fill)
+                                        .frame(maxHeight: 200)
+                                        .clipShape(RoundedRectangle(cornerRadius: Configs.cornerRadius))
+                                    
+                                } placeholder: {
+                                    ProgressView()
+                                        .frame(maxWidth: .infinity, alignment: .center)
+                                }
+                            }
+                            .padding(.bottom, 20)
+                        
+                        // 세부 내용
+                        Text(room.content)
+                            .foregroundStyle(Color.staticGray1)
+                            .padding(.bottom, 40)
+                        
+                        // 위치, 시간, 참여 인원에 대한 정보를 나타내는 뷰
+                        RoomInfoView(room: room, participantsCount: participantsCount)
+                            .padding(.bottom, 40)
+                        
+                        Text("와우, 벌써 \(participantsCount)명이나 모였어요.")
+                            .font(.title3)
+                            .fontWeight(.bold)
+                        // 참여자의 정보를 나타내는 뷰
+                        ParticipantListView(room: room, participants: participants)
+                    }
+                    .padding(.horizontal, 20)
                 }
-                .padding(.horizontal, 20)
+                .padding(.vertical, 1)
+                .padding(.bottom, 85)
+                
+                participateRoomButton
             }
-            .padding(.vertical, 1)
-            .padding(.bottom, 85)
+            .toolbarRole(.editor)
+            .onAppear {
+                Task {
+                    do {
+                        userModel = try await userService.loginedUserInfo()
+                        participants = try await roomService.joinedUsers(roomID: room.id ?? "")
+                        participantsCount = participants.count
+                    } catch {
+                        print("\(error)")
+                    }
+                }
+            }
+            .onChange(of: isLogined) {
+                if isLogined  {
+                    Task {
+                        do {
+                            isJoined = try await roomService.isJoined(roomID: room.id ?? "", userUID: userModel?.id ?? "")
+                        } catch {
+                            print("참여한 방인지 여부의 error: \(error)")
+                        }
+                    }
+                }
+            }
             
-            participateRoomButton
-        }
-        .toolbarRole(.editor)
-        .onAppear {
-            Task {
-                do {
-                    userModel = try await userService.loginedUserInfo()
-                    participants = try await roomService.joinedUsers(roomID: room.id ?? "")
-                    participantsCount = participants.count
-                } catch {
-                    print("\(error)")
+        //MARK: - iOS16
+            
+        } else {
+            ZStack(alignment: .bottom) {
+                ScrollView {
+                    LazyVStack(alignment: .leading) {
+                        // 상단 타이틀 Stack
+                        HStack {
+                            // 카테고리
+                            RoomCategoryView(room.category.rawValue)
+                            
+                            // 타이틀
+                            Text(room.title)
+                                .font(.title3)
+                        }
+                        
+                        // 썸네일 이미지
+                        RoundedRectangle(cornerRadius: 10)
+                            .foregroundStyle(.clear)
+                            .aspectRatio(contentMode: .fill)
+                            .frame(maxHeight: 200)
+                            .background {
+                                AsyncImage(url: room.roomImage) { image in
+                                    image
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fill)
+                                        .frame(maxHeight: 200)
+                                        .clipShape(RoundedRectangle(cornerRadius: Configs.cornerRadius))
+                                    
+                                } placeholder: {
+                                    ProgressView()
+                                        .frame(maxWidth: .infinity, alignment: .center)
+                                }
+                            }
+                            .padding(.bottom, 20)
+                        
+                        // 세부 내용
+                        Text(room.content)
+                            .foregroundStyle(Color.staticGray1)
+                            .padding(.bottom, 40)
+                        
+                        // 위치, 시간, 참여 인원에 대한 정보를 나타내는 뷰
+                        RoomInfoView(room: room, participantsCount: participantsCount)
+                            .padding(.bottom, 40)
+                        
+                        Text("와우, 벌써 \(participantsCount)명이나 모였어요.")
+                            .font(.title3)
+                            .fontWeight(.bold)
+                        // 참여자의 정보를 나타내는 뷰
+                        ParticipantListView(room: room, participants: participants)
+                    }
+                    .padding(.horizontal, 20)
+                }
+                .padding(.vertical, 1)
+                .padding(.bottom, 85)
+                
+                participateRoomButton
+            }
+            .toolbarRole(.editor)
+            .onAppear {
+                Task {
+                    do {
+                        userModel = try await userService.loginedUserInfo()
+                        participants = try await roomService.joinedUsers(roomID: room.id ?? "")
+                        participantsCount = participants.count
+                    } catch {
+                        print("\(error)")
+                    }
+                }
+            }
+            .onChange(of: isLogined) { newValue in
+                if isLogined  {
+                    Task {
+                        do {
+                            isJoined = try await roomService.isJoined(roomID: room.id ?? "", userUID: userModel?.id ?? "")
+                        } catch {
+                            print("참여한 방인지 여부의 error: \(error)")
+                        }
+                    }
                 }
             }
         }
@@ -107,7 +207,7 @@ struct RoomDetailView: View {
 extension RoomDetailView {
     private var participateRoomButton: some View {
         VStack {
-            GeneralButton("참여하기") {
+            GeneralButton(isJoined ? "이미 참여한 방 입니다" : "참여하기", isDisabled: isJoined) {
                 if isLogined {
                     isParticipant.toggle()
                 } else {
