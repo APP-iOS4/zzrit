11e85da8a51b464660e37c74cb0eb32382e0c3e0
Hayunho <yunho081544@gmail.com>
2024-04-23 10:23:50 +0900

    feat: ChatListView deactivate 기능 구현
    - ChatListView에서 모임 시작후 24시간 지난 방 deactivate 기능 구현
    - DetailView print문 삭제

--- ZzritConsumer/ZzritConsumer/Source/Views/Tabs/Chat/ChatListView.swift
+++ ZzritConsumer/ZzritConsumer/Source/Views/Tabs/Chat/ChatListView.swift
@@ -15,6 +15,7 @@ struct ChatListView: View {
     @State private var selection = "참여 중인 모임"
     @State private var userModel: UserModel?
     @State private var rooms: [RoomModel] = []
+    @State private var checkActivation: Bool = false
     
     private let loadRoomViewModel: LoadRoomViewModel = LoadRoomViewModel()
     
@@ -25,56 +26,92 @@ struct ChatListView: View {
     //MARK: - body
     
     var body: some View {
-        NavigationStack {
-            VStack {
-                ChatCategoryView(selection: $selection)
-                // FIXME: 현재 뷰가 Active뷰, Deactive뷰 따로 있지만 모델 연동 시, 하나의 뷰로 이용할 것, 지금은 더미로 뷰를 두 개 생성
-                if isLogined {
-                    // TODO: 가장 최근에 메시지가 온 모임이 상단에 뜨도록 정렬
-                    if selection == "참여 중인 모임" {
-                        ChatActiveListView(rooms: rooms)
-                    } else {
-                        ChatDeactiveListView(rooms: rooms)
+        if #available(iOS 17.0, *) {
+            NavigationStack {
+                VStack {
+                    ChatCategoryView(selection: $selection)
+                    // FIXME: 현재 뷰가 Active뷰, Deactive뷰 따로 있지만 모델 연동 시, 하나의 뷰로 이용할 것, 지금은 더미로 뷰를 두 개 생성
+                    if isLogined {
+                        // TODO: 가장 최근에 메시지가 온 모임이 상단에 뜨도록 정렬
+                        if selection == "참여 중인 모임" {
+                            ChatActiveListView(rooms: rooms)
+                        } else {
+                            ChatDeactiveListView(rooms: rooms)
+                        }
+                    }
+                    Spacer()
+                }
+                .toolbar {
+                    // 왼쪽 어디 탭인지 알려주는 제목
+                    ToolbarItem(placement: .topBarLeading) {
+                        HStack(spacing: 0) {
+                            Text("채팅")
+                        }
+                        .font(.title3)
+                        .fontWeight(.black)
                     }
                 }
-                Spacer()
+                .navigationBarTitleDisplayMode(.inline)
             }
-            .toolbar {
-                // 왼쪽 어디 탭인지 알려주는 제목
-                ToolbarItem(placement: .topBarLeading) {
-                    HStack(spacing: 0) {
-                        Text("채팅")
+            .onAppear {
+                Task {
+                    do {
+                        try await isLogined()
+                        try await fetchRoom()
+                        
+                        checkActivation = true
+                    } catch {
+                        print("에러 \(error)")
                     }
-                    .font(.title3)
-                    .fontWeight(.black)
                 }
-                
-                // MARK: - 의논사항: 채팅 리스트에서 검색이 필요한가?
-                
-                //                // 오른쪽 채팅 목록을 검색하는 버튼
-                //                ToolbarItem(placement: .topBarTrailing) {
-                //                    HStack(spacing: 0) {
-                //                        Button {
-                //
-                //                        } label: {
-                //                            Image(systemName: "magnifyingglass")
-                //                                .foregroundStyle(.black)
-                //                        }
-                //                    }
-                //                }
             }
-            .navigationBarTitleDisplayMode(.inline)
-        }
-        .onAppear {
-            Task {
-                do {
-                    try await isLogined()
-                    try await fetchRoom()
-                    
-                    print("rooms: \(rooms)")
-                } catch {
-                    print("에러 \(error)")
+            .onChange(of: checkActivation) {
+                deactivateRooms()
+            }
+            
+        // MARK: - iOS 16
+            
+        } else {
+            NavigationStack {
+                VStack {
+                    ChatCategoryView(selection: $selection)
+                    // FIXME: 현재 뷰가 Active뷰, Deactive뷰 따로 있지만 모델 연동 시, 하나의 뷰로 이용할 것, 지금은 더미로 뷰를 두 개 생성
+                    if isLogined {
+                        // TODO: 가장 최근에 메시지가 온 모임이 상단에 뜨도록 정렬
+                        if selection == "참여 중인 모임" {
+                            ChatActiveListView(rooms: rooms)
+                        } else {
+                            ChatDeactiveListView(rooms: rooms)
+                        }
+                    }
+                    Spacer()
                 }
+                .toolbar {
+                    // 왼쪽 어디 탭인지 알려주는 제목
+                    ToolbarItem(placement: .topBarLeading) {
+                        HStack(spacing: 0) {
+                            Text("채팅")
+                        }
+                        .font(.title3)
+                        .fontWeight(.black)
+                    }
+                }
+                .navigationBarTitleDisplayMode(.inline)
+            }
+            .onAppear {
+                Task {
+                    do {
+                        try await isLogined()
+                        try await fetchRoom()
+                        
+                        checkActivation = true
+                    } catch {
+                        print("에러 \(error)")
+                    }
+                }
+            }
+            .onChange(of: checkActivation) { newValue in
+                deactivateRooms()
             }
         }
     }
@@ -98,6 +135,18 @@ struct ChatListView: View {
         } else {
             print("로그인을 해주세요.")
         }
+    }
+    
+    func deactivateRooms() {
+        var tempRooms: [RoomModel] = []
+        
+        for room in rooms {
+            let tempRoom = loadRoomViewModel.deactivateOneRoom(room: room)
+            
+            tempRooms.append(tempRoom)
+        }
+        
+        rooms = tempRooms
     }
 }
 
