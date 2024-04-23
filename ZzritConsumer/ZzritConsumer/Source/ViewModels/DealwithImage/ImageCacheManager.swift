f0c33a553a8fb1dc446a3d554388e67ceeb71abe
yeahz <hpns1379@naver.com>
2024-04-23 15:49:37 +0900

    feat: ImageCache 메모리와 디스크 상에서 가져오도록
    이제 View에 적용하면 된다.

--- ZzritConsumer/ZzritConsumer/Source/ViewModels/DealwithImage/ImageCacheManager.swift
+++ ZzritConsumer/ZzritConsumer/Source/ViewModels/DealwithImage/ImageCacheManager.swift
@@ -10,46 +10,114 @@ import SwiftUI
 final class ImageCacheManager {
     static let shared = ImageCacheManager()
     
-    // 캐시 저장 용량 지정
     private init() {
-        // 이미지 개수 제한으로 250장
-        self.cache.countLimit = 250
-        
-        // 이미지 용량 제한으로  약 250장
+        // NSCache 저장 용량 지정
+        // 이미지 개수 제한으로 200장
+        self.cache.countLimit = 200
+        Configs.printDebugMessage("NSCache init")
+        // 이미지 용량 제한으로  약 200장
         //        self.cache.totalCostLimit = 1024 * 1024 * 250
+        
+        // filemanager 저장 위치 지정
+        do {
+            cacheDirectory = try FileManager.default.url(for: .cachesDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+        } catch {
+            fatalError("Failed to create cache directory: \(error)")
+        }
     }
     
-    // 캐시 저장 형태
+    // filemanager 저장 위치
+    private let cacheDirectory: URL
+    
+    // NSCache 저장 형태
     private let cache = NSCache<NSString, UIImage>()
     
-    // 캐시에 업데이트
-    // 1. 사용자가 사진을 올릴때
-    // 2. 필요한 사진이 캐시에 사진이 없을때
+    
+    // MARK: 이미지 저장하기
+
+    // NSCache에 업데이트
+    // TODO: updateToNSCache로 변경
     func updateToCache(name: String, image: UIImage?) {
         guard let image = image else { return }
         cache.setObject(image, forKey: name as NSString)
-        Configs.printDebugMessage("캐시에 업데이트 \(name)")
+        Configs.printDebugMessage("NSCache에 업데이트")
     }
     
-    // 캐시로부터 로드
-    // View에서 이미지를 보여줘야 할 때
-    func cachedImage(imageURL: String) -> UIImage? {
-        cache.object(forKey: imageURL as NSString)
+    // filemanager에 업데이트
+    private func updateToFileManager(name: String, image: UIImage?) {
+        // / 있으면 안되서 주소 변경
+        let encodedImageName = name.replacingOccurrences(of: "/", with: "_")
+        // 넣을 path 지정
+        let imagePath = cacheDirectory.appendingPathComponent(encodedImageName)
+        guard let imageData = image!.pngData() else { return }
+        
+        do {
+            // filemanager에 로드
+            try imageData.write(to: imagePath)
+            Configs.printDebugMessage("filemanager에 업데이트")
+        } catch {
+            Configs.printDebugMessage("Failed to save image to cache: \(error)")
+        }
+    }
+    
+    // 사용자가 처음 파베로 올릴때
+    // TODO: 바깥 세상의 updateToNSCache는 모두 updateImageFirst로 변경한다.
+    func updateImageFirst(name: String, image: UIImage?) {
+        updateToCache(name: name, image: image)
+        updateToFileManager(name: name, image: image)
+    }
+    
+    // MARK: 이미지 로드 받아오기
+    
+    // NSCache로부터 로드
+    func loadFromNSCacheImage(imageURL: String) -> UIImage? {
+        Configs.printDebugMessage("NSCache에서 불러오기")
+        return cache.object(forKey: imageURL as NSString)
+    }
+    
+    // filemanager로부터 로드
+    func loadFromFilemanagerImage(imageURL: String) -> UIImage? {
+        // / 있으면 안되서 주소 변경
+        let encodedImageName = imageURL.replacingOccurrences(of: "/", with: "_")
+        
+        // 찾을 파일 이름을 갖고 경로 설정
+        let imagePath = cacheDirectory.appendingPathComponent(encodedImageName)
+        guard FileManager.default.fileExists(atPath: imagePath.path) else { return nil }
+        
+        do {
+            Configs.printDebugMessage("filemanager에서 불러오기")
+            // 사진 찾아와서 UIImage로 return
+            let imageData = try Data(contentsOf: imagePath)
+            return UIImage(data: imageData)
+        } catch {
+            Configs.printDebugMessage("Failed to load image from cache: \(error)")
+            return nil
+        }
     }
     
     // 캐시에서 이미지 찾기
     func findImageFromCache(imageURL: String) async -> UIImage? {
-        // 캐시에 이미지가 존재했을때
-        if let image = cachedImage(imageURL: imageURL) {
-            return image
+        // NSCache에서 찾기
+        if let nsCacheImage = loadFromNSCacheImage(imageURL: imageURL) {
+            return nsCacheImage
         } else {
-            // 캐시에 이미지가 없을 때 파베로부터 다운받아오기
-            if let data = try? Data(contentsOf: URL(string: imageURL)!) {
-                // url로 부터 이미지 받아오기
-                guard let loadImageFromFB = UIImage(data: data) else { return nil }
-                // 불러온 이미지 캐시에 저장
-                updateToCache(name: imageURL, image: loadImageFromFB)
-                return loadImageFromFB
+            // filemanager에서 찾기
+            if let filemanagerImage = loadFromFilemanagerImage(imageURL: imageURL) {
+                return filemanagerImage
+            } else {
+                // 파베로부터 다운받아오기
+                if let data = try? Data(contentsOf: URL(string: imageURL)!) {
+                    // url로 부터 이미지 받아오기
+                    guard let loadImageFromFB = UIImage(data: data) else { return nil }
+                    Configs.printDebugMessage("파베에서 받아와서 ~")
+                    
+                    // filemanager 저장
+                    updateToFileManager(name: imageURL, image: loadImageFromFB)
+                    
+                    // NSCache 저장
+                    updateToCache(name: imageURL, image: loadImageFromFB)
+                    return loadImageFromFB
+                }
             }
         }
         Configs.printDebugMessage("파베에도 없음")
@@ -59,3 +127,4 @@ final class ImageCacheManager {
 
 
 // 출처 : https://velog.io/@oasis444/이미지-캐시-처리
+// 출처 : https://nsios.tistory.com/58
