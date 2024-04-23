0162cc850444ea253b78c8b9c687817fb4a94040
JaeWoongJeong <jjw8959@naver.com>
2024-04-23 14:01:50 +0900

    fix: 구글 로그인 수정
    구글 로그인시 로그인한 아이디가 전달되지 않는 문제 수정

--- ZzritConsumer/ZzritConsumer/Source/Views/Authentication/LogInView.swift
+++ ZzritConsumer/ZzritConsumer/Source/Views/Authentication/LogInView.swift
@@ -161,6 +161,8 @@ struct LogInView: View {
             } catch AuthError.noUserInfo {
                 registerdID = authService.currentUID!
                 Configs.printDebugMessage("id는 이거 : \(registerdID)")
+                print("id는 이거 : \(registerdID)")
+                googleEmailID = authService.currentUserEmail!
                 showProfile.toggle()
             } catch {
                 errorMessage = "오류가 발생했습니다.\n잠시 후 다시 시도해주시기 바랍니다."
