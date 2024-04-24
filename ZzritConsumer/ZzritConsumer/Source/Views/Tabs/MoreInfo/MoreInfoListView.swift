da73df573df0f69fa58d8d8d3aaa399ba4b57713
Hayunho <yunho081544@gmail.com>
2024-04-24 18:44:36 +0900

    fix: 더보기에서 문의하기 입력 못들어가는 버그 수정
    - 더보기 리스트 뷰에서 문의하기를 NavigationLink로 변경
    - ContactView에서 문의하기 입력 버튼 NavigationLink로 변경
    - 룸 상세 뷰 navigationDestination 형태 변경
    - ContactInputView init 중괄호 위치 수정

--- ZzritConsumer/ZzritConsumer/Source/Views/Tabs/MoreInfo/MoreInfoListView.swift
+++ ZzritConsumer/ZzritConsumer/Source/Views/Tabs/MoreInfo/MoreInfoListView.swift
@@ -21,81 +21,76 @@ struct MoreInfoListView: View {
     var isLogined: Bool
     
     var body: some View {
-        NavigationStack{
-            VStack {
-                FakeDivider()
-                // Section 1
-                VStack(alignment: .leading, spacing: 40) {
-                    // 공지사항
-                    Button {
-                        isNoticeShow.toggle()
-                    } label: {
-                        HorizontalLabel(string: "공지사항")
-                    }
-                    .navigationDestination(isPresented: $isNoticeShow){
-                        NoticeListView()
-                    }
-                    // 버전 정보
-                    HStack {
-                        Text("버전 정보")
-                        Spacer()
-                        Text("최신 버전")
-                            .foregroundStyle(Color.staticGray4)
-                    }
-                    
-                    // TODO: FAQ 넣을것인가 그것이 문제로다
-                    
-//                    // 자주 묻는 질문
-//                    Button {
-//                        
-//                    } label: {
-//                        HorizontalLabel(string: "자주 묻는 질문")
-//                        
-//                    }
-                    // 문의하기
-                    if isLogined {
-                        Button {
-                            isQuestionShow.toggle()
-                        } label: {
-                            HorizontalLabel(string: "문의하기")
-                        }
-                        .navigationDestination(isPresented: $isQuestionShow){
-                            ContactView()
-                        }
-                    }
+        VStack {
+            FakeDivider()
+            // Section 1
+            VStack(alignment: .leading, spacing: 40) {
+                // 공지사항
+                Button {
+                    isNoticeShow.toggle()
+                } label: {
+                    HorizontalLabel(string: "공지사항")
                 }
-                .padding(Configs.paddingValue)
-                FakeDivider()
-                // Section 2
-                VStack(alignment: .leading, spacing: 40) {
-                    ForEach(TermType.allCases, id: \.self) { type in
-                        Button {
-                            selectedTermType = type
-                            isTermNavigationDestination.toggle()
-                        } label: {
-                            HorizontalLabel(string: type.title)
-                        }
-                    }
-
+                .navigationDestination(isPresented: $isNoticeShow){
+                    NoticeListView()
                 }
-                .padding(Configs.paddingValue)
+                // 버전 정보
+                HStack {
+                    Text("버전 정보")
+                    Spacer()
+                    Text("최신 버전")
+                        .foregroundStyle(Color.staticGray4)
+                }
+                
+                // TODO: FAQ 넣을것인가 그것이 문제로다
+                
+                //                    // 자주 묻는 질문
+                //                    Button {
+                //
+                //                    } label: {
+                //                        HorizontalLabel(string: "자주 묻는 질문")
+                //
+                //                    }
+                // 문의하기
                 if isLogined {
-                    FakeDivider()
-                    // Section 3
-                    VStack(alignment: .leading, spacing: 40) {
-                        // 로그아웃
-                        Button {
-                            isShowingAlert.toggle()
-                        } label: {
-                            HorizontalLabel(string: "로그아웃")
-                                .foregroundStyle(.red)
-                        }
+                    NavigationLink {
+                        ContactView()
+                    } label: {
+                        HorizontalLabel(string: "문의하기")
                     }
-                    .padding(Configs.paddingValue)
                 }
             }
-            .foregroundStyle(Color.staticGray1)
+            .padding(Configs.paddingValue)
+            FakeDivider()
+            // Section 2
+            VStack(alignment: .leading, spacing: 40) {
+                ForEach(TermType.allCases, id: \.self) { type in
+                    Button {
+                        selectedTermType = type
+                        isTermNavigationDestination.toggle()
+                    } label: {
+                        HorizontalLabel(string: type.title)
+                    }
+                }
+                
+            }
+            .padding(Configs.paddingValue)
+            if isLogined {
+                FakeDivider()
+                // Section 3
+                VStack(alignment: .leading, spacing: 40) {
+                    // 로그아웃
+                    Button {
+                        isShowingAlert.toggle()
+                    } label: {
+                        HorizontalLabel(string: "로그아웃")
+                            .foregroundStyle(.red)
+                    }
+                }
+                .padding(Configs.paddingValue)
+            }
         }
+        .foregroundStyle(Color.staticGray1)
         .navigationDestination(isPresented: $isTermNavigationDestination) {
             TermView(type: selectedTermType)
         }
@@ -105,11 +100,11 @@ struct MoreInfoListView: View {
             } label: {
                 Text("로그아웃")
             }
-
+            
         } message: {
             Text("정말 로그아웃 하시겠습니까?")
         }
-
+        
     }
     
     private func logout() {
