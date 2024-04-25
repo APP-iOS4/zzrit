4e97533dfa827dce580be318c71fd3ff9540da2a
iosHumhae <ios.humhae@gmail.com>
2024-04-25 11:08:06 +0900

    remove: 기존 주석처리 된 함수 제거

--- ZzritKit/Sources/ZzritKit/Service/StorageService.swift
+++ ZzritKit/Sources/ZzritKit/Service/StorageService.swift
@@ -30,28 +30,6 @@ public final class StorageService {
     lazy var storageReference = Storage.storage().reference()
     
     public init() { }
-//    
-//    /// FirebaseStorage에 이미지를 업로드 합니다.
-//    /// - Parameter topDir(FirebaseConstants.StorageName): 최상위 폴더 열거형
-//    /// - Parameter dirs([String]): 서브 디렉토리 이름 배열
-//    /// - Parameter image(Data): 업로드 할 Data 타입의 이미지
-//    /// - Returns (String): 업로드 된 이미지의 String 타입의 URL
-//    public func imageUpload(topDir: StorageName, dirs: [String], image: Data) async throws -> String {
-//        do {
-//            var rf = storageReference.child(topDir.rawValue)
-//            
-//            for dir in dirs {
-//                rf = rf.child(dir)
-//            }
-//            
-//            let _ = try await rf.putDataAsync(image)
-//            let downloadURL = try await rf.downloadURL().absoluteString
-//            return downloadURL
-//        } catch {
-//            throw error
-//        }
-//    }
-    
     
     /// FirebaseStorage에 이미지를 업로드 합니다.
     /// - Parameters:
