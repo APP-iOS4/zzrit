a38f8eec06eb8847220626ee058a6c7617b7f684
SunJoon Lee <sunjoon96@gmail.com>
2024-04-29 10:56:32 +0900

    fix: 회원가입 페이지 스크롤 뷰 추가

--- ZzritConsumer/ZzritConsumer/Source/Views/Authentication/SignUpView.swift
+++ ZzritConsumer/ZzritConsumer/Source/Views/Authentication/SignUpView.swift
@@ -29,85 +29,87 @@ struct SignUpView: View {
     
     var body: some View {
         NavigationStack {
-            VStack {
-                AppSlogan()
-                
-                Spacer(minLength: 50)
-                
-                UserInputCellView(text: $signUpId, title: "이메일", symbol: "envelope")
-                    .customOnChange(of: signUpId) { _ in
-                        activeSignUpButton()
-                    }
-                
-                // MARK: 이메일 형식 체크 오류 메시지
-                if !signUpId.isEmpty {
-                    if checkEmail(signUpId){
-                        SpaceErrorTextView()
-                    } else {
-                        ErrorTextView(title: "이메일 형식이 올바르지 않습니다.")
-                    }
-                } else {
-                    ErrorTextView(title: "이메일 형식에 맞게 입력해주세요.")
-                }
-                UserInputCellView(text: $signUpPw1, title: "비밀번호", symbol: "lock", isPassword: true)
-                    .customOnChange(of: signUpPw1) { _ in
-                        activeSignUpButton()
-                    }
-                UserInputCellView(text: $signUpPw2, title: "비밀번호 확인", symbol: "lock", isPassword: true)
-                    .customOnChange(of: signUpPw2) { _ in
-                        activeSignUpButton()
-                    }
-                
-                // MARK: 비밀번호 일치 여부 확인 메시지
-                if !equlText {
-                    if signUpPw1.isEmpty || signUpPw2.isEmpty {
-                        ErrorTextView(title: "비밀번호 입력란은 모두 채워주세요.")
-                    } else {
-                        ErrorTextView(title: "비밀번호가 일치하지 않습니다.")
-                    }
-                } else {
-                    if signUpPw1.count < 8 || signUpPw2.count < 8 {
-                        ErrorTextView(title: "8글자 이상의 영어, 숫자, 문자로 입력해주세요.")
-                    }else {
-                        ErrorTextView(title: " ")
-                    }
-                }
-                
-                // MARK: 이용약관 동의 버튼
-                SignUpAgreeButton(selectAgree: $selectAgree)
-                    .onTapGesture {
-                        if selectAgree {
-                            selectAgree = false
+            ScrollView {
+                VStack {
+                    AppSlogan()
+                    
+                    Spacer(minLength: 50)
+                    
+                    UserInputCellView(text: $signUpId, title: "이메일", symbol: "envelope")
+                        .customOnChange(of: signUpId) { _ in
+                            activeSignUpButton()
+                        }
+                    
+                    // MARK: 이메일 형식 체크 오류 메시지
+                    if !signUpId.isEmpty {
+                        if checkEmail(signUpId){
+                            SpaceErrorTextView()
                         } else {
-                            agreeSheet.toggle()
+                            ErrorTextView(title: "이메일 형식이 올바르지 않습니다.")
+                        }
+                    } else {
+                        ErrorTextView(title: "이메일 형식에 맞게 입력해주세요.")
+                    }
+                    UserInputCellView(text: $signUpPw1, title: "비밀번호", symbol: "lock", isPassword: true)
+                        .customOnChange(of: signUpPw1) { _ in
+                            activeSignUpButton()
+                        }
+                    UserInputCellView(text: $signUpPw2, title: "비밀번호 확인", symbol: "lock", isPassword: true)
+                        .customOnChange(of: signUpPw2) { _ in
+                            activeSignUpButton()
+                        }
+                    
+                    // MARK: 비밀번호 일치 여부 확인 메시지
+                    if !equlText {
+                        if signUpPw1.isEmpty || signUpPw2.isEmpty {
+                            ErrorTextView(title: "비밀번호 입력란은 모두 채워주세요.")
+                        } else {
+                            ErrorTextView(title: "비밀번호가 일치하지 않습니다.")
+                        }
+                    } else {
+                        if signUpPw1.count < 8 || signUpPw2.count < 8 {
+                            ErrorTextView(title: "8글자 이상의 영어, 숫자, 문자로 입력해주세요.")
+                        }else {
+                            ErrorTextView(title: " ")
                         }
                     }
-                    .sheet(isPresented: $agreeSheet, content: {
-                        ServiceAgreeSheet(selectAgree: $selectAgree, agreeSheet: $agreeSheet)
-                    })
-                    .padding(.top, Configs.paddingValue)
-                
-                // MARK: 회원가입 버튼
-                GeneralButton("회원가입", isDisabled: !isSignUpButtonActive) {
-                    register()
+                    
+                    // MARK: 이용약관 동의 버튼
+                    SignUpAgreeButton(selectAgree: $selectAgree)
+                        .onTapGesture {
+                            if selectAgree {
+                                selectAgree = false
+                            } else {
+                                agreeSheet.toggle()
+                            }
+                        }
+                        .sheet(isPresented: $agreeSheet, content: {
+                            ServiceAgreeSheet(selectAgree: $selectAgree, agreeSheet: $agreeSheet)
+                        })
+                        .padding(.top, Configs.paddingValue)
+                    
+                    // MARK: 회원가입 버튼
+                    GeneralButton("회원가입", isDisabled: !isSignUpButtonActive) {
+                        register()
+                    }
+                    .customOnChange(of: selectAgree) { _ in
+                        activeSignUpButton()
+                    }
+                    .navigationDestination(isPresented: $showProfile) {
+                        SetProfileView(isTopDismiss: $isTopDismiss, emailField: signUpId, registeredUID: $registeredUID)
+                    }
+                    
+                    Spacer(minLength: 50)
+                    
+                    AuthenticationButton(type: .login) {
+                        dismiss()
+                    }
                 }
-                .customOnChange(of: selectAgree) { _ in
-                    activeSignUpButton()
-                }
-                .navigationDestination(isPresented: $showProfile) {
-                    SetProfileView(isTopDismiss: $isTopDismiss, emailField: signUpId, registeredUID: $registeredUID)
-                }
-                
-                Spacer(minLength: 50)
-                
-                AuthenticationButton(type: .login) {
-                    dismiss()
-                }
+                .padding(Configs.paddingValue)
+                .toolbarRole(.editor)
             }
-            .padding(Configs.paddingValue)
-            .toolbarRole(.editor)
+            .loading(isLoading, message: "계정을 생성하고 있습니다.")
         }
-        .loading(isLoading, message: "계정을 생성하고 있습니다.")
     }
     
     func activeSignUpButton() {
